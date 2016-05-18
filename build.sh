f28c4d4dc342477d7f318b5b0f285af76513fd04
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-19 05:55:33 +1000

    Add search directories for Linux (Ubuntu)

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 #
 MODULE=GLib-2.0
 export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
+GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
 GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
-GIR_DIR=${GOBJECT_DIR}/share/gir-1.0
-GIR=${GIR_DIR}/${MODULE}.gir
+for prefix in $PREFIX GOBJECT_DIR /usr/local /usr ; do
+	gir_dir=${prefix}/share/gir-1.0
+	gir=${gir_dir}/${MODULE}.gir
+	if [ -e "${gir}" ] ; then
+		export GIR=${gir}
+		export GIR_DIR=${gir_dir}
+	fi
+done
 if [ ! -e "${GIR}" ] ; then
 	echo "*** ${GIR} does not exist!"
-	echo "Make sure gobject-introspection is installed"
-	echo "and can be found by pkg-config!"
+	echo "Make sure libgirepository1.0-dev is installed"
+	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
 LINKFLAGS=`pkg-config --libs glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
