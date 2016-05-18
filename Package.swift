cc056193acd291551c53185f4ddad26ef1b2d74e
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-19 07:22:51 +1000

    Fix typo in repository name

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GLib",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CGlib.git", majorVersion: 1)
+        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
     ]
 )
