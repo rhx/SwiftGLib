136aac9a1de933b718aa5b4749c9e02638dbbc8b
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-21 07:18:06 +1000

    Use GitHub URL

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GLib",
     dependencies: [
-        //.Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
-        .Package(url: "../CGLib", majorVersion: 1)
+        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
     ]
 )
