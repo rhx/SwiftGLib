c28bca465ddd5c511baf367a291929a61ccd2f9f
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-21 06:55:20 +1000

    Use local CGLib

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GLib",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
+        //.Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
+        .Package(url: "../CGLib", majorVersion: 1)
     ]
 )
