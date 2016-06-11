03fd34d71d7c638c7153a99c266ccc32d704bb1e
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-12 06:17:21 +1000

    Split out wrapper generator script from build.sh

--- build.sh
+++ build.sh
@@ -3,23 +3,6 @@
 MODULE=GLib-2.0
 module=`echo "${MODULE}" | tr '[:upper:]' '[:lower:]'`
 export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
-GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
-for prefix in $PREFIX GOBJECT_DIR /usr/local /usr ; do
-	gir_dir=${prefix}/share/gir-1.0
-	gir=${gir_dir}/${MODULE}.gir
-	if [ -e "${gir}" ] ; then
-		export GIR=${gir}
-		export GIR_DIR=${gir_dir}
-	fi
-done
-if [ ! -e "${GIR}" ] ; then
-	echo "*** ${GIR} does not exist!"
-	echo "Make sure libgirepository1.0-dev is installed"
-	echo "and can be found in /usr /usr/local or by pkg-config!"
-	exit 1
-fi
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module gio-unix-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
-gir2swift "${GIR}" > Sources/${MODULE}.swift
-swift build $CCFLAGS $LINKFLAGS "$@"
+exec swift build $CCFLAGS $LINKFLAGS "$@"
---
+++ generate-wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+MODULE=GLib-2.0
+GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
+GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
+for prefix in $PREFIX GOBJECT_DIR /usr/local /usr ; do
+	gir_dir=${prefix}/share/gir-1.0
+	gir=${gir_dir}/${MODULE}.gir
+	if [ -e "${gir}" ] ; then
+		export GIR=${gir}
+		export GIR_DIR=${gir_dir}
+	fi
+done
+if [ ! -e "${GIR}" ] ; then
+	echo "*** ${GIR} does not exist!"
+	echo "Make sure libgirepository1.0-dev is installed"
+	echo "and can be found in /usr /usr/local or by pkg-config!"
+	exit 1
+fi
+exec gir2swift "${GIR}" > Sources/${MODULE}.swift
