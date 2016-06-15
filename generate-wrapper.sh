a6bf66cfbf63f2a021f333c3b05e69a96aac3aae
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-15 11:34:49 +1000

    Move recursive package fetching out of gir-to-swift.sh

--- generate-wrapper.sh
+++ generate-wrapper.sh
@@ -3,4 +3,6 @@
 # Recursive swift code wrapper generator for gobject-introspection (.gir) files.
 # This calls the non-recursive gir-to-swift.sh to do the heavy lifting.
 #
+. ./config.sh
+./package.sh fetch
 . ./gir-to-swift.sh "$@"
