df3968b3631985a7126c798194f82dee61385dc3
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-15 11:19:39 +1000

    Split out non-recursive gir-to-swift.sh

--- generate-wrapper.sh
+++ generate-wrapper.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 #
-# Swift code wrapper generator for gobject-introspection (.gir) files.
-# This needs an installed `gir2swift' executable (github.com/rhx/gir2swift)
-# as well as awk.
+# Recursive swift code wrapper generator for gobject-introspection (.gir) files.
+# This calls the non-recursive gir-to-swift.sh to do the heavy lifting.
 #
-. ./config.sh
-./package.sh fetch
-mkdir -p Sources
-GOBJECT_LIBDIR="`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`"
-GOBJECT_DIR="`dirname "${GOBJECT_LIBDIR}"`"
-for prefix in /usr /usr/local $GOBJECT_DIR $PREFIX ; do
-	gir_dir=${prefix}/share/gir-1.0
-	gir=${gir_dir}/${Module}.gir
-	if [ -e "${gir}" ] ; then
-		export GIR=${gir}
-		export GIR_DIR=${gir_dir}
-	fi
-done
-if [ ! -e "${GIR}" ] ; then
-	echo "*** ${GIR} does not exist!"
-	echo "Make sure libgirepository1.0-dev is installed"
-	echo "and can be found in /usr /usr/local or by pkg-config!"
-	exit 1
-fi
-exec gir2swift "${GIR}" | awk -f ${Module}.awk > Sources/${Module}.swift
+. ./gir-to-swift.sh "$@"
