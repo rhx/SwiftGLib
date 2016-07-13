11928559153de8079843e19364810e08afaa5d62
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-13 15:13:32 +1000

    Remove hard-coded PATH

--- config.sh
+++ config.sh
@@ -6,6 +6,5 @@
 Mod=GLib
 Module=${Mod}-2.0
 module="`echo "${Module}" | tr '[:upper:]' '[:lower:]'`"
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
 export LINKFLAGS="`pkg-config --libs $module gio-unix-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`"
 export CCFLAGS="`pkg-config --cflags $module gio-unix-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `"
