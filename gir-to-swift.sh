d0f5f4b2738249dd5749440f056a486574b50427
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-31 12:31:11 +1000

    Compile with Swift 3.0 preview 3

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Swift code wrapper generator for gobject-introspection (.gir) files.
 # This needs an installed `gir2swift' executable (github.com/rhx/gir2swift)
 # as well as awk.
 #
 . ./config.sh
+./package.sh fetch
 mkdir -p Sources
 GOBJECT_LIBDIR="`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`"
 GOBJECT_DIR="`dirname "${GOBJECT_LIBDIR}"`"
@@ -22,4 +23,11 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
+if ! pushd Packages/gir2swift >/dev/null 2>&1 ; then
+	pushd Packages >/dev/null
+	git clone https://github.com/rhx/gir2swift.git
+	cd gir2swift && ./build.sh
+fi
+export PATH=`pwd`/.build/debug:${PATH}
+popd >/dev/null
 exec gir2swift "${GIR}" | sed -f ${Module}.sed | awk -f ${Module}.awk > Sources/${Module}.swift
