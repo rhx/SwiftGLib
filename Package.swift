95af3b17351e6800dbc2fce25638355364322f58
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 06:20:56 +1000

    Update for Swift 4

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "GLib",
     dependencies: [
         .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1)
-    ]
+    ],
+    swiftLanguageVersions: [3, 4]
 )
