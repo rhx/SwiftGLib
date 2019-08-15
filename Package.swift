b2ae5a15a059a62dfc1ba83593eb90e4535ac173
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-15 17:09:45 +1000

    Remove CGLib dependency and squelch warning about deprecated system packages

--- Package.swift
+++ Package.swift
@@ -8,11 +8,13 @@ let package = Package(
     products: [
 	.library(name: "GLib", targets: ["GLib"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/rhx/CGLib.git", .branch("master"))
-    ],
     targets: [
-        .target(name: "GLib", dependencies: []),
+	.systemLibrary(name: "CGLib", pkgConfig: "glib-2.0 gio-unix-2.0",
+	    providers: [
+		.brew(["glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	    ]),
+        .target(name: "GLib", dependencies: ["CGLib"]),
         .testTarget(name: "GLibTests", dependencies: ["GLib"]),
     ]
 )
