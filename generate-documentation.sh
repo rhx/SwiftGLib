05104210f326e8bb7ce87f3973d460087a8f3805
Rene Hexel <r.hexel@griffith.edu.au>
2019-09-30 12:01:20 +1000

    Add jazzy documentation generator

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -6,7 +6,9 @@
 . ./config.sh
 [ -e Sources/${Module}.swift ] || ./generate-wrapper.sh
 if [ -z "$@" ]; then
-    JAZZY_ARGS='--theme fullwidth --author "Rene Hexel" --author_url "https://www.ict.griffith.edu.au/~rhexel/" --github_url "https://github.com/rhx/SwiftGLib" --github-file-prefix https://github.com/rhx/SwiftGLib/tree/generated --root-url http://rhx.github.io/SwiftGLib/ --output docs'
+    JAZZY_ARGS='--theme fullwidth --author "Rene Hexel" --author_url https://www.ict.griffith.edu.au/~rhexel/ --github_url https://github.com/rhx/SwiftGLib --github-file-prefix https://github.com/rhx/SwiftGLib/tree/generated --root-url http://rhx.github.io/SwiftGLib/ --output docs'
 fi
+rm -rf .docs.old
+mv docs .docs.old 2>/dev/null
 jazzy --swift-build-tool spm --clean --module-version $JAZZY_VER	\
       --module $Mod $JAZZY_ARGS "$@" -b $JAZZY_B
