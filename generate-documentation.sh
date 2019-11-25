398b2cdda8708b4952cfccb98222d1785d664c5e
Rene Hexel <r.hexel@griffith.edu.au>
2019-11-26 06:13:22 +1000

    Separate sourcekitten and jazzy

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -8,7 +8,9 @@
 if [ -z "$@" ]; then
     JAZZY_ARGS="--theme fullwidth --author Ren&eacute;&nbsp;Hexel --author_url https://www.ict.griffith.edu.au/~rhexel/ --github_url https://github.com/rhx/Swift$Mod --github-file-prefix https://github.com/rhx/Swift$Mod/tree/generated --root-url http://rhx.github.io/Swift$Mod/ --output docs"
 fi
+[ -e .build/build.db ] || ./build.sh
 rm -rf .docs.old
 mv docs .docs.old 2>/dev/null
-jazzy --swift-build-tool spm --clean --module-version $JAZZY_VER	\
-      --module $Mod $JAZZY_ARGS "$@" -b $JAZZY_B
+sourcekitten doc --spm-module $Mod -- $CCFLAGS $LINKFLAGS > .build/$Mod-doc.json
+jazzy --sourcekitten-sourcefile .build/$Mod-doc.json --clean            \
+      --module-version $JAZZY_VER --module $Mod $JAZZY_ARGS "$@"
