f55188455f0c95d924350e0f6e581d7b21f82ce1
Rene Hexel <r.hexel@griffith.edu.au>
2020-01-31 10:13:51 +1000

    Work around leftover GDesktopAppLaunchCallback in .gif files

--- Sources/CGLib/glib_bridging.h
+++ Sources/CGLib/glib_bridging.h
@@ -268,3 +268,12 @@ typedef struct _GObjectNotifyQueue
     guint16               n_pspecs;
     guint16               freeze_count;
 } GObjectNotifyQueue;
+
+#ifndef __G_DESKTOP_APP_INFO_H__
+struct _GDesktopAppInfoClass {};
+typedef struct _GDesktopAppInfo        GDesktopAppInfo;
+typedef struct _GDesktopAppInfoClass   GDesktopAppInfoClass;
+typedef void (*GDesktopAppLaunchCallback) (GDesktopAppInfo  *appinfo,
+                                           GPid              pid,
+                                           gpointer          user_data);
+#endif
