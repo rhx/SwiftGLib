23f6ade28001939dd7692ec9c2d3ff71af77adc6
Rene Hexel <r.hexel@griffith.edu.au>
2020-06-14 20:54:12 +1000

    More idiomatic Swift support (requires gir2swift version 9)

--- Sources/GLib/GLib.swift
+++ Sources/GLib/GLib.swift
@@ -10,18 +10,35 @@ import CGLib
 /// Opaque type. See RecMutexLocker for details.
 public struct GRecMutexLocker {}
 
+public extension LogLevelFlags {
+    /// log level for debug messages, see `g_debug()`
+    static let debug = LogLevelFlags.levelDebug
+    /// log level for informational messages, see `g_info()`
+    static let info = LogLevelFlags.levelInfo
+    /// log level for warnings, see `g_warning()`
+    static let warning = LogLevelFlags.levelWarning
+    /// log level for critical warning messages, see
+    ///     `g_critical()`.
+    ///     This level is also used for messages produced by `g_return_if_fail()`
+    ///     and `g_return_val_if_fail()`.
+    static let critical = LogLevelFlags.levelCritical
+    /// log level for errors, see `g_error()`.
+    ///     This level is also used for messages produced by `g_assert()`.
+    static let error = LogLevelFlags.levelError
+}
+
 #if !os(Linux)
 /// Logging function
 ///
 /// - Parameters:
 ///   - message: log message
-///   - level: log level (defaults to `level_debug`)
-public func g_log(_ message: String, level: LogLevelFlags = .level_debug) {
+///   - level: log level (defaults to `.debug`)
+public func g_log(_ message: String, level: LogLevelFlags = .debug) {
     message.utf8CString.withUnsafeBufferPointer {
         guard let base = $0.baseAddress else { return }
         let varargs = [OpaquePointer(base)]
         withVaList(varargs) {
-            g_logv(nil, level, "%s", $0)
+            g_logv(nil, level.value, "%s", $0)
         }
     }
 }
@@ -31,13 +48,13 @@ public func g_log(_ message: String, level: LogLevelFlags = .level_debug) {
 /// - Parameters:
 ///   - domain: the domain this logging function occurs in
 ///   - message: log message
-///   - level: log level (defaults to `level_debug`)
-public func g_log(domain: String, _ message: String, level: LogLevelFlags = .level_debug) {
+///   - level: log level (defaults to `.debug`)
+public func g_log(domain: String, _ message: String, level: LogLevelFlags = .debug) {
     message.utf8CString.withUnsafeBufferPointer {
         guard let base = $0.baseAddress else { return }
         let varargs = [OpaquePointer(base)]
         withVaList(varargs) {
-            g_logv(domain, level, "%s", $0)
+            g_logv(domain, level.value, "%s", $0)
         }
     }
 }
@@ -46,8 +63,8 @@ public func g_log(domain: String, _ message: String, level: LogLevelFlags = .lev
 ///
 /// - Parameters:
 ///   - message: log message
-///   - level: log level (defaults to `level_debug`)
-public func g_log(_ message: String, level: LogLevelFlags = .level_debug) {
+///   - level: log level (defaults to `.debug`)
+public func g_log(_ message: String, level: LogLevelFlags = .debug) {
     var buffer = message
     if message.index(of: "%") != nil {
         buffer = message.reduce("") { $0 + ($1 == "%" ? "%%" : String($1)) }
@@ -55,7 +72,7 @@ public func g_log(_ message: String, level: LogLevelFlags = .level_debug) {
     withUnsafeMutableBytes(of: &buffer) {
         guard let buffer = $0.baseAddress else { return }
         let msg = buffer.assumingMemoryBound(to: CChar.self)
-        g_logv(nil, level, msg, CVaListPointer(_fromUnsafeMutablePointer: buffer))
+        g_logv(nil, level.value, msg, CVaListPointer(_fromUnsafeMutablePointer: buffer))
     }
 }
 
@@ -64,8 +81,8 @@ public func g_log(_ message: String, level: LogLevelFlags = .level_debug) {
 /// - Parameters:
 ///   - domain: the domain this logging function occurs in
 ///   - message: log message
-///   - level: log level (defaults to `level_debug`)
-public func g_log(domain: String, _ message: String, level: LogLevelFlags = .level_debug) {
+///   - level: log level (defaults to `.debug`)
+public func g_log(domain: String, _ message: String, level: LogLevelFlags = .debug) {
     var buffer = message
     if message.index(of: "%") != nil {
         buffer = message.reduce("") { $0 + ($1 == "%" ? "%%" : String($1)) }
@@ -73,7 +90,7 @@ public func g_log(domain: String, _ message: String, level: LogLevelFlags = .lev
     withUnsafeMutableBytes(of: &buffer) {
         guard let buffer = $0.baseAddress else { return }
         let msg = buffer.assumingMemoryBound(to: CChar.self)
-        g_logv(domain, level, msg, CVaListPointer(_fromUnsafeMutablePointer: buffer))
+        g_logv(domain, level.value, msg, CVaListPointer(_fromUnsafeMutablePointer: buffer))
     }
 }
 #endif
@@ -85,8 +102,8 @@ public func g_log(domain: String, _ message: String, level: LogLevelFlags = .lev
 ///   - domain: the domain this logging occurs in (defaults to `nil`)
 public func g_warning(_ message: String, domain: String? = nil) {
     if let d = domain {
-        g_log(domain: d, message, level: .level_warning)
+        g_log(domain: d, message, level: .warning)
     } else {
-        g_log(message, level: .level_warning)
+        g_log(message, level: .warning)
     }
 }
