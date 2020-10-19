d554d132329099f0a08471f0aff99e725acd4b70
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-10-19 22:59:40 +0200

    Remove the need for .build

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "GLib",
     products: [ .library(name: "GLib", targets: ["GLib"]) ],
     targets: [
-        .systemLibrary(name: "CGLib", pkgConfig: "glib-2.0 gio-unix-2.0",
+        .systemLibrary(name: "CGLib", pkgConfig: "gio-unix-2.0",
             providers: [
                 .brew(["glib", "glib-networking", "gobject-introspection"]),
                 .apt(["libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
